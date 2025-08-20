be5ceff0a11206ad3848aed851691ce4c2ecd3c9
leramalkevich <leramalkevich@gmail.com>
2025-08-20 15:07:43 +0300

    sign-in reactive form

--- src/app/shared/components/sign-in/sign-in.component.ts
+++ src/app/shared/components/sign-in/sign-in.component.ts
@@ -1,16 +1,25 @@
-import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Component, ElementRef, inject, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {SignService} from '../../services/sign.service';
 import {CommonModule} from '@angular/common';
+import {FormBuilder, ReactiveFormsModule, Validators} from '@angular/forms';
+import {PopUpService} from '../../services/pop-up.service';
 
 @Component({
   selector: 'sign-in',
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './sign-in.component.html',
   styleUrl: './sign-in.component.scss'
 })
 export class SignInComponent implements OnInit, OnDestroy {
+  private fb = inject(FormBuilder);
   signInIsShown:boolean = false;
-  constructor(private signService:SignService) {
+  @ViewChild('password') password!: ElementRef;
+  signInForm = this.fb.group({
+    email:['', [Validators.required, Validators.email]],
+    password:['', Validators.required],
+    keepSigned:[false,[]]
+  });
+  constructor(private signService:SignService, private popUpService:PopUpService) {
   }
   ngOnInit() {
     this.signService.signInIsShown$.subscribe((isShown:boolean)=>{
@@ -20,9 +29,24 @@ export class SignInComponent implements OnInit, OnDestroy {
   ngOnDestroy() {
     this.signService.hideSignIn();
   }
+  togglePassword() {
+    if (this.password.nativeElement.getAttribute('type') == 'password') {
+      this.password.nativeElement.setAttribute('type', 'text');
+    } else {
+      this.password.nativeElement.setAttribute('type', 'password');
+    }
+  }
 
   hideSignIn(){
     this.signService.hideSignIn();
+    this.signInForm.reset();
+    this.signInForm.markAsUntouched();
+  }
+  showPopUp(){
+    if (this.signInForm.value.email && this.signInForm.value.password) {
+      this.hideSignIn();
+      this.popUpService.showPopUp.set(true);
+    }
   }
   showSignUp(){
     this.signService.hideSignIn();
